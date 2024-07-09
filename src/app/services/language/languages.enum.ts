79b15edca47ed990427ad4f7a666f23c0e6ed0f8
RaydAlsayf <raydalsayf@gmail.com>
2024-07-09 23:58:15 +0300

    add comments

--- src/app/services/language/languages.enum.ts
+++ src/app/services/language/languages.enum.ts
@@ -1,3 +1,8 @@
+/*
+* The languages enum
+* This file contains the default language, supported languages, and the default language direction
+*/
+
 export const defaultLanguage: string = "ar";
 export const defaultLanguageDirection: string = "rtl";
 export const supportedLanguages: string[] = ["ar", "en", "fr"];
@@ -6,3 +11,10 @@ export const supportedLanguagesDirection : { [key: string]: string } = {
     ar: "rtl",
     fr: "ltr",
 };
+
+/**
+ * if you need to add a new language, 
+ * 1. add the language code to the supportedLanguages array.
+ * 2. you need to create a new translation file in src/assets/i18n/ (e.g., newLang.json).
+ * 3. Update your components to include translations for the new language.
+ */
